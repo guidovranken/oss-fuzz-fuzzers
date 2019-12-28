f2765f5fbbf1c16e587985a48d4dba2d92afa56a
Guido Vranken <guidovranken@gmail.com>
2019-12-28 05:16:15 +0100

    Pillow fuzzer: Copy required shared object files

--- pillow/loadimg.py
+++ pillow/loadimg.py
@@ -1,4 +1,11 @@
 import sys, os, warnings
+import subprocess
+from shutil import copyfile
+
+copyfile(os.path.dirname(sys.executable) + '/libjpeg.so.8', '/usr/lib/x86_64-linux-gnu/libjpeg.so.8')
+copyfile(os.path.dirname(sys.executable) + '/libtiff.so.5', '/usr/lib/x86_64-linux-gnu/libtiff.so.5')
+copyfile(os.path.dirname(sys.executable) + '/libjbig.so.0', '/usr/lib/x86_64-linux-gnu/libjbig.so.0')
+
 sys.path.append(os.path.join(os.path.dirname(sys.path[-1]), 'pillow/src/'))
 warnings.simplefilter("ignore")
 
